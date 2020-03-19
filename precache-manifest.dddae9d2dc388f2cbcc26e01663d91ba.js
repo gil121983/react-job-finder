0ba16b886c874631dba758db171cf31c68c812f6
gil121983 <gilstolar@icloud.com>
2020-03-19 13:25:05 +0200

    Updates

---
+++ precache-manifest.dddae9d2dc388f2cbcc26e01663d91ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896a3c2c1ae2eef3d0595a500056a782",
+    "url": "/react-job-finder/index.html"
+  },
+  {
+    "revision": "9d2b4125e7fb05cb35d7",
+    "url": "/react-job-finder/static/css/main.718e9e7a.chunk.css"
+  },
+  {
+    "revision": "e918d334c31bf7a3da9b",
+    "url": "/react-job-finder/static/js/2.9d16e00b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-job-finder/static/js/2.9d16e00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2b4125e7fb05cb35d7",
+    "url": "/react-job-finder/static/js/main.9ed441d5.chunk.js"
+  },
+  {
+    "revision": "8848df2fb2254273e012",
+    "url": "/react-job-finder/static/js/runtime-main.fa3da2d3.js"
+  }
+]);
